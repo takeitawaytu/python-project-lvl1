0bf8ed5f4be5fb7fbb54f1b3cc5dec0249f6d80b
asnigirev <51997250+asnigirev@users.noreply.github.com>
2021-07-06 14:54:58 +0300

    add linter to project

--- Makefile
+++ Makefile
@@ -15,3 +15,6 @@ brain-games:
 package-install:
 	python -m pip install --user dist/hexlet_code-0.1.1-py3-none-any.whl
 	
+lint:
+	poetry run flake8 brain_games
+	
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,8 +5,10 @@ def welcome_user():
     name = prompt.string('May i have your name? ')
     return print('Hello, {}!'.format(name))
 
+
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
