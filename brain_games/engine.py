ef02abe5489cc3fe43efbb4ed0b2f0d1fc03fc9f
asnigirev <51997250+asnigirev@users.noreply.github.com>
2021-07-07 14:39:21 +0300

    add engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,27 @@
+import prompt
+from brain_games.cli import welcome_user
+from brain_games.scripts.brain_games import greet
+
+
+def game_engine(questions=''):
+    greet()
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for question in questions:
+        data, correct_answer = question
+        print('Question: {}'.format(data))
+        answer = prompt.string('Your answer: ')
+        if answer != correct_answer:
+            print('"{}" is wrong answer ;(. Correct answer was "{}".'.format(answer, correct_answer))
+            return print('Let\'s try again, {}!'.format(name))
+        elif answer == correct_answer:
+            print('Correct!')
+    return print('Congratulations, {}!'.format(name))
+
+
+def main():
+    game_engine()
+
+
+if __name__ == '__main__':
+    main()
